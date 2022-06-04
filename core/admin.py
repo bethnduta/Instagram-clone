509963172fa2cf98a5e948adcd9df11d584f7272
bethnduta <bethnduta05@gmail.com>
2022-06-04 05:49:31 -0700

    modified the static and templates

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import post
+from .models import *
 
 # Register your models here.
 admin.site.register(post)
+# admin.site.register(Profile)
+# admin.site.register(comment)
+# admin.site.register(like)
+# admin.site.register(follow)
