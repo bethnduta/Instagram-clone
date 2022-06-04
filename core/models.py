f54acee60610cd350e8a783d7b7463f9e02c60a2
bethnduta <bethnduta05@gmail.com>
2022-06-04 01:14:23 -0700

    properties for the post model

--- core/models.py
+++ core/models.py
@@ -1,9 +1,13 @@
+from email.mime import image
 from django.db import models
+from pytz import timezone
 
 # Create your models here.
 class post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', blank=True,null=True)
+    caption=models.TextField()
+    created_date=(models.DateTimeField(default=timezone.now))
 
     def __str__(self):
         return self.title
