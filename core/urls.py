e43ed70656be1b4e1feb4aa4d082a4dcb29f365b
bethnduta <bethnduta05@gmail.com>
2022-06-05 08:13:46 -0700

    imported postCreateView

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from .views import postListView
+from .views import postCreateView
 
 urlpatterns = [
     path('', postListView.as_view(), name='index'),
+    path('new/', postCreateView.as_view(), name='post_create'),
 ]
