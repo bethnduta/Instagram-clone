ff35dcd999342fcf243ca7f480b72b084f441a31
bethnduta <bethnduta05@gmail.com>
2022-06-05 10:25:21 -0700

    added testcase

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from . models import post
 # Create your tests here.
+class PostTest(TestCase):
+    def test_str(self):
+        test_title = post(caption='A picture')
+        self.assertEqual(str(test_title),'A picture')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(post(),post))    
