4d732747b8d0e654f45ba669c7398ba63a5457ed
bethnduta <bethnduta05@gmail.com>
2022-06-05 10:31:13 -0700

    models

--- core/models.py
+++ core/models.py
@@ -10,3 +10,31 @@ class post(models.Model):
 
     def __str__(self):
         return self.caption
+
+
+# class Comment(models.Model):
+#     content = models.TextField()
+#     image = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+#     def __str__(self) -> str:
+#         return f"{self.content}"
+
+
+# class Like(models.Model):
+#     image = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+#     def __str__(self) -> str:
+#         return f"{self.user.username} Likes"
+
+
+# #Create followers and follow
+# class Follow(models.Model):
+#     account_following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers', null=True)
+#     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower', null=True)
+
+#     def __str__(self) -> str:
+#         return f"{self.follower}"        
